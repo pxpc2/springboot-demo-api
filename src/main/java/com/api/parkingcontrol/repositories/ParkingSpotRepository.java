ba099ce14373ccbdde5c054301db385ecf410559
pxpc2 <pedro_daia@hotmail.com>
2022-09-22 11:35:50 -0300

    preparing basic repository interface

--- src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -1,2 +1,16 @@
-package com.api.parkingcontrol.repositories;public interface ParkingSpotRepository {
+package com.api.parkingcontrol.repositories;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository
+        extends JpaRepository<ParkingSpotModel, UUID>
+{
+
+
+
 }
