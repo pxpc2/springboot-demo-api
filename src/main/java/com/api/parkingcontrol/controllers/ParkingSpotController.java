922bf2487a94911e9b8899c39ead5796650d921e
pxpc2 <pedro_daia@hotmail.com>
2022-09-22 12:32:42 -0300

    creating POST request from controller -> service -> repository

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -1,5 +1,41 @@
-package com.api.parkingcontrol.controllers;/**
-* @Author Pedro Daia
-*/
-public class ParkingSpotController {
+package com.api.parkingcontrol.controllers;
+
+import com.api.parkingcontrol.dtos.ParkingSpotDTO;
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.services.ParkingSpotService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController
+{
+
+    private final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(final ParkingSpotService parkingSpotService)
+    {
+        this.parkingSpotService = parkingSpotService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Object> addParkingSpot(
+            @RequestBody @Valid final ParkingSpotDTO parkingSpotDTO)
+    {
+        final var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDTO, parkingSpotModel);
+        parkingSpotModel.setCheckinDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                parkingSpotService.add(parkingSpotModel)
+        );
+    }
+
 }
--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,5 +1,27 @@
-package com.api.parkingcontrol.services;/**
-* @Author Pedro Daia
-*/
-public class ParkingSpotService {
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class ParkingSpotService
+{
+
+    private final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(final ParkingSpotRepository parkingSpotRepository)
+    {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+
+    @Transactional
+    public ParkingSpotModel add(final ParkingSpotModel parkingSpotModel)
+    {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
 }
